2ef130702a9ed603f0fda024f54f61451ad466fe
Eduardo Quagliato <eduardo@quagliato.me>
2016-10-21 15:55:21 -0200

    v0.0.2 -- ending connections

--- mariadb.js
+++ mariadb.js
@@ -46,64 +46,59 @@ var MariaDB = function(sessionConfig, sessionLogger){
 
   mariadbObj = this;
 
-  this.connectDB = function(callback){
-    if (mariadbObj.dbConnection === null || mariadbObj.dbConnection === undefined) {
-      mariadbObj.dbConnection = mysql.createConnection({
-        host     : mariadbObj.dbConfig.DB_HOST,
-        user     : mariadbObj.dbConfig.DB_USER,
-        password : mariadbObj.dbConfig.DB_PASS,
-        database : mariadbObj.dbConfig.DB_NAME
-      });
-      mariadbObj.logger("Instanced MariaDB Connection.");
+  this.connectDB = function(){
+    var conn = mysql.createConnection({
+      host     : mariadbObj.dbConfig.DB_HOST,
+      user     : mariadbObj.dbConfig.DB_USER,
+      password : mariadbObj.dbConfig.DB_PASS,
+      database : mariadbObj.dbConfig.DB_NAME
+    });
+    mariadbObj.logger("Instanced MariaDB Connection.");
 
-      mariadbObj.dbConnection.connect();
-      mariadbObj.logger("Connected to MariaDB Server");
+    conn.connect();
+    mariadbObj.logger("Connected to MariaDB Server");
 
-      callback(mariadbObj.dbConnection);
-      return;
-    }
-
-    callback(mariadbObj.dbConnection);
+    return conn;
   };
 
-  this.disconnectDB = function(){
+  this.disconnectDB = function(conn){
     mariadbObj.logger("Connection to MariaDB Server ended.");
-    mariadbObj.dbConnection.end();
+    conn.end();
   };
 
-  this.query = function(queryStr, callback){
+ this.query = function(queryStr, callback){
     mariadbObj.logger("SQL: {0}".format(queryStr));
-    mariadbObj.connectDB(function(connection){
-      connection.query(queryStr, function(err, rows, fields) {
-        if (err) {
-          mariadbObj.logger("SQL: {0} - {1}".format(queryStr, err), "CRITICAL");
-          callback(-1);
-        } else {
-          var size = 0;
-          var is_select = true;
-          if (rows.affectedRows>0) { // Insert, update, delete
-            is_select = false;
-          } else if (rows && rows != null && rows.length > 0) {
-            size = rows.length;
-          }
-
-          var result = [];
-
-          if (size > 0) {
-            for (var i = 0; i < rows.length; i++) {
-              var newRow = {};
-              for (var j = 0; j < fields.length; j++) {
-                newRow[fields[j].name]= rows[i][fields[j].name]
-              }
-              result[result.length] = newRow
-            }
-          }
-
-          if (size === 1) result = result[0];
-
-          callback(parseInt(size), result);
+    var connection = mariadbObj.connectDB();
+    connection.query(queryStr, function(err, rows, fields) {
+      if (err) {
+        mariadbObj.logger("SQL: {0} - {1}".format(queryStr, err), "CRITICAL");
+        callback(-1);
+      } else {
+        var size = 0;
+        var is_select = true;
+        if (rows.affectedRows>0) { // Insert, update, delete
+          is_select = false;
+        } else if (rows && rows != null && rows.length > 0) {
+          size = rows.length;
         }
-      });
+
+        var result = [];
+
+        if (size > 0) {
+          for (var i = 0; i < rows.length; i++) {
+            var newRow = {};
+            for (var j = 0; j < fields.length; j++) {
+              newRow[fields[j].name]= rows[i][fields[j].name]
+            }
+            result[result.length] = newRow
+          }
+        }
+
+        if (size === 1) result = result[0];
+      }
+      
+      callback(parseInt(size), result);
+      mariadbObj.disconnectDB(connection);
     });
   };
 };
